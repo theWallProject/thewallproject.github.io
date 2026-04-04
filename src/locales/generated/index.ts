8062a5c057562cbc5dda5a72350f718453abcc49
TheWall <the.wall.addon@proton.me>
2026-04-04 05:37:33 +0200

    Fix formatting of import statements in index.ts for consistency

--- src/locales/generated/index.ts
+++ src/locales/generated/index.ts
@@ -1,15 +1,15 @@
 // Auto-generated from JSON files
 // Do not edit manually - run 'npm run generate-i18n' to regenerate
 
-import en from "./en.json" assert { type: "json" };
-import ar from "./ar.json" assert { type: "json" };
-import fr from "./fr.json" assert { type: "json" };
-import es from "./es.json" assert { type: "json" };
-import de from "./de.json" assert { type: "json" };
-import id from "./id.json" assert { type: "json" };
-import nl from "./nl.json" assert { type: "json" };
-import zh_CN from "./zh_CN.json" assert { type: "json" };
-import zh_TW from "./zh_TW.json" assert { type: "json" };
+import en from './en.json' assert { type: 'json' };
+import ar from './ar.json' assert { type: 'json' };
+import fr from './fr.json' assert { type: 'json' };
+import es from './es.json' assert { type: 'json' };
+import de from './de.json' assert { type: 'json' };
+import id from './id.json' assert { type: 'json' };
+import nl from './nl.json' assert { type: 'json' };
+import zh_CN from './zh_CN.json' assert { type: 'json' };
+import zh_TW from './zh_TW.json' assert { type: 'json' };
 
 export const resources = {
   en,
@@ -20,7 +20,7 @@ export const resources = {
   id,
   nl,
   zh_CN,
-  zh_TW,
+  zh_TW
 } as const;
 
 export default resources;
